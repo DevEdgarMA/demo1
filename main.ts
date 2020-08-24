d02cb5cdd437f098f7c59392ab33703c205e5ba6
Edgar Eucebio Mamani Apaza <47419561+DevEdgarMA@users.noreply.github.com>
2020-08-24 09:55:25 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+scene.setBackgroundColor(9)
+let hero = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f . . . . . . 
+    . . . . . f . . . f . . . . . . 
+    . . . . . f . . . f . . . . . . 
+    . . . . . f . . . f . . . . . . 
+    . . . . . f f f f f . . . . . . 
+    . . . . . . . f . . . . . . . . 
+    . . . . . . . f . . . . . . . . 
+    . . . . . . . f . . . . . . . . 
+    . . . . . . . f . . . . . . . . 
+    . . . . . . . f . . . . . . . . 
+    . . . . f f f f f f f . . . . . 
+    . . . . f f f f f f f . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+controller.moveSprite(hero)
